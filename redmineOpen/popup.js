ea6da715c40546779e55800b861c098500cb9bb3
msobczak <msobczak@artegence.com>
2016-01-22 18:08:16 +0100

    tłumaczenia do popupa

--- redmineOpen/popup.js
+++ redmineOpen/popup.js
@@ -2,6 +2,8 @@ var root = chrome.extension.getBackgroundPage();
 
 $(function() {
 
+	localizeHtmlPage();
+	
 	$('#issueId').focus();
 	document.execCommand('paste');
 	document.execCommand('selectAll');
@@ -126,3 +128,24 @@ function execDefaultAction(){
 
 var defaultAction = 'showIssue';
 
+
+function localizeHtmlPage()
+{
+    //Localize by replacing __MSG_***__ meta tags
+    var objects = document.getElementsByTagName('html');
+    for (var j = 0; j < objects.length; j++)
+    {
+        var obj = objects[j];
+
+        var valStrH = obj.innerHTML.toString();
+        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1)
+        {
+            return v1 ? chrome.i18n.getMessage(v1) : "";
+        });
+
+        if(valNewH != valStrH)
+        {
+            obj.innerHTML = valNewH;
+        }
+    }
+}
